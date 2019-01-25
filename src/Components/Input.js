ce71baf79f56b6f9f81104fc637abf2c9e565c7c
Joseph Lander <josephclander@gmail.com>
2019-01-25 17:15:54 -0500

    added handleChange method to all inputs and updated outputs to change

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -17,43 +17,101 @@ class Input extends Component {
           <div class="form__container">
             <label>
               Full Name:
-              <input type="text" name="fullName" 
-                value={this.props.data.fullName} 
-                onChange={this.props.handleChange} />
+              <input
+                type="text"
+                name="fullName"
+                value={this.props.data.fullName}
+                onChange={this.props.handleChange}
+              />
             </label>
             <label>
               Email:
-              <input type="email" name="email" />
+              <input
+                type="email"
+                name="email"
+                value={this.props.data.email}
+                onChange={this.props.handleChange}
+              />
             </label>
             <label>
               Phone:
-              <input type="tel" name="phone" />
+              <input
+                type="tel"
+                name="phone"
+                value={this.props.data.phone}
+                onChange={this.props.handleChange}
+              />
             </label>
             <label>
               LinkedIn:
-              <input type="text" name="linkedIn" />
+              <input
+                type="text"
+                name="linkedIn"
+                value={this.props.data.linkedIn}
+                onChange={this.props.handleChange}
+              />
             </label>
           </div>
           <br />
           <h3 className="form__subheader">Main Skills</h3>
           <p>e.g. Forward Thinking, Team Player, Resourceful</p>
           <div class="form__container">
-            <input type="text" name="skill-1" placeholder="1" />
-            <input type="text" name="skill-2" placeholder="2" />
-            <input type="text" name="skill-3" placeholder="3" />
-            <input type="text" name="skill-4" placeholder="4" />
-            <input type="text" name="skill-5" placeholder="5" />
+            <input
+              type="text"
+              name="skill1"
+              placeholder="1"
+              value={this.props.data.skill1}
+              onChange={this.props.handleChange}
+            />
+            <input
+              type="text"
+              name="skill2"
+              placeholder="2"
+              value={this.props.data.skill2}
+              onChange={this.props.handleChange}
+            />
+            <input
+              type="text"
+              name="skill3"
+              placeholder="3"
+              value={this.props.data.skill3}
+              onChange={this.props.handleChange}
+            />
+            <input
+              type="text"
+              name="skill4"
+              placeholder="4"
+              value={this.props.data.skill4}
+              onChange={this.props.handleChange}
+            />
+            <input
+              type="text"
+              name="skill5"
+              placeholder="5"
+              value={this.props.data.skill5}
+              onChange={this.props.handleChange}
+            />
           </div>
           <br />
           <h3 className="form__subheader">Education</h3>
           <div class="form__container">
             <label>
               Degree Title:
-              <input type="text" name="degreeTitle" />
+              <input
+                type="text"
+                name="degreeTitle"
+                value={this.props.data.degreeTitle}
+                onChange={this.props.handleChange}
+              />
             </label>
             <label>
               Institution:
-              <input type="text" name="institution" />
+              <input
+                type="text"
+                name="institution"
+                value={this.props.data.institution}
+                onChange={this.props.handleChange}
+              />
             </label>
             <label>
               <br />
@@ -64,9 +122,17 @@ class Input extends Component {
                 type="number"
                 name="yearFrom"
                 placeholder="format - 2002"
+                value={this.props.data.yearFrom}
+                onChange={this.props.handleChange}
               />
               Year To:{" "}
-              <input type="number" name="yearTo" placeholder="format - 2005" />
+              <input
+                type="number"
+                name="yearTo"
+                placeholder="format - 2005"
+                value={this.props.data.yearTo}
+                onChange={this.props.handleChange}
+              />
             </label>
           </div>
           <br />
@@ -76,6 +142,8 @@ class Input extends Component {
             name="profile"
             id="profile"
             placeholder="Use this space to give a brief decription of what makes you a good choice for the company you are applying for."
+            value={this.props.data.profile}
+            onChange={this.props.handleChange}
           />
           <br />
           <h3 className="form__subheader">Experience</h3>
@@ -83,11 +151,21 @@ class Input extends Component {
           <div class="form__container">
             <label>
               Job Title:
-              <input type="text" name="jobTitle1" />
+              <input
+                type="text"
+                name="jobTitle1"
+                value={this.props.data.jobTitle1}
+                onChange={this.props.handleChange}
+              />
             </label>
             <label>
               Company:
-              <input type="text" name="company1" />
+              <input
+                type="text"
+                name="company1"
+                value={this.props.data.company1}
+                onChange={this.props.handleChange}
+              />
             </label>
             <label>
               <br />
@@ -98,31 +176,73 @@ class Input extends Component {
                 type="number"
                 name="yearFromPos1"
                 placeholder="format - 2002"
+                value={this.props.data.yearFromPos1}
+                onChange={this.props.handleChange}
               />
               Year To:{" "}
               <input
                 type="number"
                 name="yearToPos1"
                 placeholder="format - 2005"
+                value={this.props.data.yearToPos1}
+                onChange={this.props.handleChange}
               />
             </label>
-            <textarea
-              className="form__input"
-              name="pos1info"
-              id="pos1info"
-              placeholder="Breifly explain your achievements at this post. State how this experience would be an asset to the company you are applying for."
-            />
+            <label>
+              Brief Description:
+              <br />
+              <textarea
+                className="form__input"
+                name="pos1info"
+                id="pos1info"
+                placeholder="Breifly explain your achievements at this post. State how this experience would be an asset to the company you are applying for."
+                value={this.props.data.pos1info}
+                onChange={this.props.handleChange}
+              />
+            </label>
+            <label>
+              Bullet Point 1:
+              <br />
+              <textarea
+                className="form__input"
+                name="pos1bullet1"
+                id="pos1info"
+                value={this.props.data.pos1bullet1}
+                onChange={this.props.handleChange}
+              />
+            </label>
+            <label>
+              Bullet Point 2:
+              <br />
+              <textarea
+                className="form__input"
+                name="pos1bullet2"
+                id="pos1info"
+                value={this.props.data.pos1bullet2}
+                onChange={this.props.handleChange}
+              />
+            </label>
           </div>
           <br />
           <h6>Position 2:</h6>
           <div class="form__container">
             <label>
               Job Title:
-              <input type="text" name="jobTitle2" />
+              <input
+                type="text"
+                name="jobTitle2"
+                value={this.props.data.jobTitle2}
+                onChange={this.props.handleChange}
+              />
             </label>
             <label>
               Company:
-              <input type="text" name="company2" />
+              <input
+                type="text"
+                name="company2"
+                value={this.props.data.company2}
+                onChange={this.props.handleChange}
+              />
             </label>
             <label>
               <br />
@@ -133,20 +253,52 @@ class Input extends Component {
                 type="number"
                 name="yearFromPos2"
                 placeholder="format - 2002"
+                value={this.props.data.yearFromPos2}
+                onChange={this.props.handleChange}
               />
               Year To:{" "}
               <input
                 type="number"
                 name="yearToPos2"
                 placeholder="format - 2005"
+                value={this.props.data.yearToPos2}
+                onChange={this.props.handleChange}
               />
             </label>
-            <textarea
-              className="form__input"
-              name="pos2info"
-              id="pos1info"
-              placeholder="Breifly explain your achievements at this post. State how this experience would be an asset to the company you are applying for."
-            />
+            <label>
+              Brief Description
+              <br />
+              <textarea
+                className="form__input"
+                name="pos2info"
+                id="pos2info"
+                placeholder="Breifly explain your achievements at this post. State how this experience would be an asset to the company you are applying for."
+                value={this.props.data.pos2info}
+                onChange={this.props.handleChange}
+              />
+            </label>
+            <label>
+              Bullet Point 1:
+              <br />
+              <textarea
+                className="form__input"
+                name="pos2bullet1"
+                id="pos2info"
+                value={this.props.data.pos2bullet1}
+                onChange={this.props.handleChange}
+              />
+            </label>
+            <label>
+              Bullet Point 2:
+              <br />
+              <textarea
+                className="form__input"
+                name="pos2bullet2"
+                id="pos2info"
+                value={this.props.data.pos2bullet2}
+                onChange={this.props.handleChange}
+              />
+            </label>
           </div>
         </form>
       </div>
--- src/Components/Preview.js
+++ src/Components/Preview.js
@@ -8,110 +8,78 @@ class Preview extends Component {
         <div className="preview__header border">{this.props.data.fullName}</div>
         <div className="info">
           <div class="info__value">
-            <p className="email">josephclander@gmail.com</p>
+            <p className="email">{this.props.data.email}</p>
             <i class="fas fa-envelope-square" />
           </div>
           <div class="info__value">
-            <p className="phone">+1-347-517-6347</p>
+            <p className="phone">{this.props.data.phone}</p>
             <i class="fas fa-phone-square" />
           </div>
           <div class="info__value">
-            <p className="linkedin">/josephclander</p>
+            <p className="linkedin">/{this.props.data.linkedIn}</p>
             <i class="fab fa-linkedin" />
           </div>
         </div>
         <div className="skills">
           <h2 className="preview__subheading">Skills</h2>
           <ul className="skills__list">
-            <li>Patience</li>
-            <li>Attention to detail</li>
-            <li>Planning</li>
-            <li>Team player</li>
-            <li>Efficient</li>
+            <li>{this.props.data.skill1}</li>
+            <li>{this.props.data.skill2}</li>
+            <li>{this.props.data.skill3}</li>
+            <li>{this.props.data.skill4}</li>
+            <li>{this.props.data.skill5}</li>
           </ul>
         </div>
         <div className="education">
           <h2 className="preview__subheading">Education</h2>
-          <h5 className="education__title">
-            Mathematics with Actuarial Studies BSc
-          </h5>
-          <p>Southampton University</p>
-          <p>1999 - 2002</p>
+          <h5 className="education__title">{this.props.data.degreeTitle}</h5>
+          <p>{this.props.data.institution}</p>
+          <p>
+            {this.props.data.yearFrom} - {this.props.data.yearTo}
+          </p>
         </div>
         <div className="profile">
           <h2 className="preview__subheading">Professional Profile</h2>
-          <p>
-            A previous deputy head of Mathematics and head of A-Level
-            Mathematics, for one of the largest secondary schools, in the top
-            100 schools, in the UK. 13 years experience leading large teams of
-            experts to educate cohorts of 300+ students. Always staying aware of
-            government proposed changes to education, ensuring teams were
-            trained for new content and the curriculum was delivered seamlessly.
-            Innovative techniques and technologies were used to enpower students
-            and teachers alike to make progress.
-          </p>
+          <p>{this.props.data.profile}</p>
         </div>
         <div className="work-experience">
           <h2 className="preview__subheading">Experience</h2>
           <div class="work-experience__position">
             <h5 className="work-experience__title">
-              Deputy Head of Mathematics
+              {this.props.data.jobTitle1}
             </h5>
             <h6 className="work-experience__compDate">
-              Graveney School / 2014 - 2018
+              {this.props.data.company1} / {this.props.data.yearFromPos1} -{" "}
+              {this.props.data.yearToPos1}
             </h6>
             <p>
-              My role had me assist in leading a team of 20 technical
-              professionals, a third of the members of which having more than 20
-              years experience. I was respected by the head of department to
-              conduct appraisals for team members and was in charge of new
-              teacher training in partnership with one of the UK's leading
-              training institutions, Kings College University London.
+              {this.props.data.pos1info}
             </p>
             <ul>
               <li>
-                Pushed incremental, test driven development to monitor the
-                outcomes of the students, which became a model for the school.
+                {this.props.data.pos1bullet1}
               </li>
               <li>
-                Respected by the department to train senior staff to utilize
-                technology and to support junior staff with technical content,
-                keeping all working together and supporting one another.
+                {this.props.data.pos1bullet2}
               </li>
             </ul>
           </div>
           <div class="work-experience__position">
             <h5 className="work-experience__title">
-              Head of A-Level Mathematics & ICT Coordinator
+              {this.props.data.jobTitle2}
             </h5>
             <h6 className="work-experience__compDate">
-              Graveney School / 2008 - 2018
+              {this.props.data.company2} / {this.props.data.yearFromPos2} - {this.props.data.yearToPos2}
             </h6>
             <p>
-              In my first few years I took the initiative to improve the
-              department's provision of ICT for shared data and resources for
-              teachers and created a single HTML page to keep people updated
-              with new additions. This led to delivering whole school
-              presentations of how to manage and share content and being brought
-              into the IT department and selected to test the first ever CMS
-              being rolled out to schools across the UK. In further years, I
-              worked with my deputy to update our provision for a changing
-              curriculum. We worked hard to implement changes considering;
-              training a department, UX design of our setup and further improved
-              our provision to students via our own automated data management
-              system.
+              {this.props.data.pos2info}
             </p>
             <ul>
               <li>
-                I learned of Agile philosphy and completed trials with students
-                to implement scrum techniques. Thus giving me insight into the
-                benefits and struggles of working under this ethos.
+                {this.props.data.pos2bullet1}
               </li>
               <li>
-                Tests showed student attention spans didn't stretch even up to
-                10 minutes when self studying from videos so I created my own
-                YouTube Channel "miniMaths" with concepts explained in under 3
-                minutes.
+                {this.props.data.pos2bullet2}
               </li>
             </ul>
           </div>
